aab9fcc12be093fbba4cb0acd87b686192c6ad3f
VIL20776 <vil20776@uvg.edu.gt>
2024-05-19 12:19:13 -0600

    first commit

---
+++ Hbase.py
@@ -0,0 +1,245 @@
+from matplotlib import table
+from Hfiles import Table, read_hfile, write_hfile
+
+class HBaseSimulator:
+    def __init__(self):
+        self.tables = {}
+        
+    def load_table(self, table: Table):
+        if table.name in self.tables:
+            print(f"Table {table.name} already exists.")
+        else:
+            self.tables[table.name] = table
+            print(f"Table {table.name} loaded successfully.")
+    
+    def save_table(self, table_name):
+        if table_name not in self.tables:
+            print(f"Table {table_name} does not exists.")
+        else:
+            write_hfile(self.tables[table_name])
+            print(f"Table {table_name} saved successfully.")
+
+    def create_table(self, table_name, column_families):
+        if table_name in self.tables:
+            print(f"Table {table_name} already exists.")
+        else:
+            self.tables[table_name] = Table(table_name, column_families)
+            print(f"Table {table_name} created successfully.")
+
+    def list_tables(self):
+        if not self.tables:
+            print("No tables available.")
+        else:
+            for table in self.tables.keys():
+                print(table)
+
+    def disable_table(self, table_name):
+        if table_name in self.tables:
+            self.tables[table_name].disable()
+            print(f"Table {table_name} disabled.")
+        else:
+            print(f"Table {table_name} does not exist.")
+
+    def is_enabled(self, table_name):
+        if table_name in self.tables:
+            if self.tables[table_name].enabled:
+                print(f"Table {table_name} is enabled.")
+            else:
+                print(f"Table {table_name} is disabled.")
+        else:
+            print(f"Table {table_name} does not exist.")
+
+    def alter_table(self, table_name, column_families):
+        if table_name in self.tables:
+            self.tables[table_name].alter(column_families)
+            print(f"Table {table_name} altered successfully.")
+        else:
+            print(f"Table {table_name} does not exist.")
+
+    def drop_table(self, table_name):
+        if table_name in self.tables:
+            del self.tables[table_name]
+            print(f"Table {table_name} dropped successfully.")
+        else:
+            print(f"Table {table_name} does not exist.")
+
+    def drop_all_tables(self):
+        self.tables.clear()
+        print("All tables dropped successfully.")
+
+    def describe_table(self, table_name):
+        if table_name in self.tables:
+            self.tables[table_name].describe()
+        else:
+            print(f"Table {table_name} does not exist.")
+
+    def put(self, table_name, row_key, column_family, column, value):
+        if table_name in self.tables:
+            self.tables[table_name].put(row_key, column_family, column, value)
+        else:
+            print(f"Table {table_name} does not exist.")
+
+    def get(self, table_name, row_key):
+        if table_name in self.tables:
+            self.tables[table_name].get(row_key)
+        else:
+            print(f"Table {table_name} does not exist.")
+
+    def scan(self, table_name):
+        if table_name in self.tables:
+            self.tables[table_name].scan()
+        else:
+            print(f"Table {table_name} does not exist.")
+
+    def delete(self, table_name, row_key, column_family, column):
+        if table_name in self.tables:
+            self.tables[table_name].delete(row_key, column_family, column)
+        else:
+            print(f"Table {table_name} does not exist.")
+
+    def delete_all(self, table_name, row_key):
+        if table_name in self.tables:
+            self.tables[table_name].delete_all(row_key)
+        else:
+            print(f"Table {table_name} does not exist.")
+
+    def count(self, table_name):
+        if table_name in self.tables:
+            count = self.tables[table_name].count()
+            print(f"Table {table_name} has {count} rows.")
+        else:
+            print(f"Table {table_name} does not exist.")
+
+    def truncate(self, table_name):
+        if table_name in self.tables:
+            self.tables[table_name].disable()
+            column_families = self.tables[table_name].column_families
+            del self.tables[table_name]
+            self.tables[table_name] = Table(table_name, column_families)
+            print(f"Table {table_name} truncated successfully.")
+        else:
+            print(f"Table {table_name} does not exist.")
+
+
+
+def main():
+    simulator = HBaseSimulator()
+    while True:
+        print("\n1. DDL (Lenguaje de definición de datos)")
+        print("2. DML (Lenguaje de manipulación de datos)")
+        print("3. Cargar/Guardar archivos")
+        print("4. Salir")
+        choice = input("Seleccione una opción: ")
+        if choice == "1":
+            while True:
+                print("\nDDL (Lenguaje de definición de datos)")
+                print("1. Create")
+                print("2. List")
+                print("3. Disable")
+                print("4. Is_enabled")
+                print("5. Alter")
+                print("6. Drop")
+                print("7. Drop All")
+                print("8. Describe")
+                print("9. Volver al menú principal")
+                ddl_choice = input("Seleccione una opción de DDL: ")
+                if ddl_choice == "1":
+                    table_name = input("Ingrese el nombre de la tabla: ")
+                    column_families = input("Ingrese las column families separadas por comas: ").split(",")
+                    simulator.create_table(table_name, column_families)
+                elif ddl_choice == "2":
+                    simulator.list_tables()
+                elif ddl_choice == "3":
+                    table_name = input("Ingrese el nombre de la tabla: ")
+                    simulator.disable_table(table_name)
+                elif ddl_choice == "4":
+                    table_name = input("Ingrese el nombre de la tabla: ")
+                    simulator.is_enabled(table_name)
+                elif ddl_choice == "5":
+                    table_name = input("Ingrese el nombre de la tabla: ")
+                    column_families = input("Ingrese las nuevas column families separadas por comas: ").split(",")
+                    simulator.alter_table(table_name, column_families)
+                elif ddl_choice == "6":
+                    table_name = input("Ingrese el nombre de la tabla: ")
+                    simulator.drop_table(table_name)
+                elif ddl_choice == "7":
+                    simulator.drop_all_tables()
+                elif ddl_choice == "8":
+                    table_name = input("Ingrese el nombre de la tabla: ")
+                    simulator.describe_table(table_name)
+                elif ddl_choice == "9":
+                    break
+                else:
+                    print("Opción no válida.")
+        elif choice == "2":
+            while True:
+                print("\nDML (Lenguaje de manipulación de datos)")
+                print("1. Put")
+                print("2. Get")
+                print("3. Scan")
+                print("4. Delete")
+                print("5. Deleteall")
+                print("6. Count")
+                print("7. Truncate")
+                print("8. Volver al menú principal")
+                dml_choice = input("Seleccione una opción de DML: ")
+                if dml_choice == "1":
+                    table_name = input("Ingrese el nombre de la tabla: ")
+                    row_key = input("Ingrese el row key: ")
+                    column_family = input("Ingrese la column family: ")
+                    column = input("Ingrese la columna: ")
+                    value = input("Ingrese el valor: ")
+                    simulator.put(table_name, row_key, column_family, column, value)
+                elif dml_choice == "2":
+                    table_name = input("Ingrese el nombre de la tabla: ")
+                    row_key = input("Ingrese el row key: ")
+                    simulator.get(table_name, row_key)
+                elif dml_choice == "3":
+                    table_name = input("Ingrese el nombre de la tabla: ")
+                    simulator.scan(table_name)
+                elif dml_choice == "4":
+                    table_name = input("Ingrese el nombre de la tabla: ")
+                    row_key = input("Ingrese el row key: ")
+                    column_family = input("Ingrese la column family: ")
+                    column = input("Ingrese la columna: ")
+                    simulator.delete(table_name, row_key, column_family, column)
+                elif dml_choice == "5":
+                    table_name = input("Ingrese el nombre de la tabla: ")
+                    row_key = input("Ingrese el row key: ")
+                    simulator.delete_all(table_name, row_key)
+                elif dml_choice == "6":
+                    table_name = input("Ingrese el nombre de la tabla: ")
+                    simulator.count(table_name)
+                elif dml_choice == "7":
+                    table_name = input("Ingrese el nombre de la tabla: ")
+                    simulator.truncate(table_name)
+                elif dml_choice == "8":
+                    break
+                else:
+                    print("Opción no válida.")
+        elif choice == "3":
+            while True:
+                print("\nCargar/Guardar archivos")
+                print("1. Cargar")
+                print("2. Guardar")
+                print("3. Regresar")
+                sl_choice = input("Seleccione una opcion: ")
+                if sl_choice == '1':
+                    file_path = input("\nEscriba la ruta del archivo: ")
+                    table = read_hfile(file_path)
+                    simulator.load_table(table)
+                elif sl_choice == '2':
+                    table_name = input("\nEscriba el nombre de la tabla a guardar: ")
+                    simulator.save_table(table_name)
+                elif sl_choice == '3':
+                    break
+                else:   
+                    print("Opción no válida.")
+            
+        elif choice == "4":
+            break
+        else:
+            print("Opción no válida.")
+
+if __name__ == "__main__":
+    main()
---
+++ Hfiles.py
@@ -0,0 +1,91 @@
+import json
+import datetime
+from pathlib import Path
+
+class Table:
+    def __init__(self, name, column_families, rows = {}):
+        self.name = name
+        self.column_families = column_families
+        self.rows = rows
+        self.enabled = True  
+
+    def disable(self):
+        self.enabled = False
+
+    def enable(self):
+        self.enabled = True
+
+    def alter(self, column_families):
+        self.column_families = column_families
+
+    def describe(self):
+        print(f"Table: {self.name}")
+        print(f"Column Families: {', '.join(self.column_families)}")
+
+    def put(self, row_key, column_family, column, value):
+        if column_family not in self.column_families:
+            print(f"Column family {column_family} does not exist.")
+            return
+        if row_key not in self.rows:
+            self.rows[row_key] = {}
+        if column_family not in self.rows[row_key]:
+            self.rows[row_key][column_family] = {}
+        if column not in self.rows[row_key][column_family]:
+            self.rows[row_key][column_family][column] = {}
+        timestamp = datetime.datetime.now()
+        self.rows[row_key][column_family][column][timestamp.strftime("%d/%m/%Y %H:%M:%S")] = value
+        print(f"Inserted/Updated row {row_key} in table {self.name}.")
+
+    def get(self, row_key):
+        if row_key in self.rows:
+            print(f"Row {row_key} in table {self.name}:")
+            for cf, columns in self.rows[row_key].items():
+                for col, ts in columns.items():
+                    for t, val in ts.items():
+                        print(f"{cf}:{col} = {val} (timestamp: {t})")
+        else:
+            print(f"Row {row_key} does not exist in table {self.name}.")
+
+    def scan(self):
+        print(f"Scanning table {self.name}:")
+        for row_key in self.rows.keys():
+            self.get(row_key)
+
+    def delete(self, row_key, column_family, column):
+        if row_key in self.rows and column_family in self.rows[row_key] and column in self.rows[row_key][column_family]:
+            del self.rows[row_key][column_family][column]
+            print(f"Deleted column {column_family}:{column} from row {row_key} in table {self.name}.")
+        else:
+            print(f"Column {column_family}:{column} does not exist in row {row_key}.")
+
+    def delete_all(self, row_key):
+        if row_key in self.rows:
+            del self.rows[row_key]
+            print(f"Deleted row {row_key} from table {self.name}.")
+        else:
+            print(f"Row {row_key} does not exist in table {self.name}.")
+
+    def count(self):
+        return len(self.rows)
+
+
+def read_hfile(file):
+    f = open(file)
+    
+    name = Path(file).stem
+    data = json.load(f)
+    column_family = []
+    
+    for cf in data.values():
+        column_family = cf.keys()
+        break
+    
+    table = Table(name, column_family, data)
+    
+    return table
+
+def write_hfile(table: Table):
+    f = open("regions/" + table.name + ".hfile", 'w')
+    json.dump(table.rows, f)
+    
+    
